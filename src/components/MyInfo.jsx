2593d2a04374a9096d895f8e64bf665708f04070
lewno <cabax.2002@gmail.com>
2023-07-16 23:00:50 -0300

    agregando mis habilidades

--- src/components/MyInfo.jsx
+++ src/components/MyInfo.jsx
@@ -1,10 +1,12 @@
 import AboutMe from "./AboutMe"
+import MySkills from "./MySkills"
 
 const MyInfo = () => {
   return (
     <section className="w-full m-auto text-white">
         <div className="w-[80%] 2xl:w-[70%] m-auto">
             <AboutMe/>
+            <MySkills/>
         </div>
     </section>
   )
