b8504d18b571ed5621e7e535e81fb6e0982c3521
lewno <cabax.2002@gmail.com>
2023-08-16 20:16:01 -0300

    iconos de svg a componentes y comienzo de la darkmode

---
+++ src/assets/icons-svg/icons.jsx
@@ -0,0 +1,370 @@
+export const Sun = ({ className, ...rest }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    {...rest}
+    className={`w-full h-auto ${className}`}
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <g strokeDasharray="2">
+        <path d="M12 21v1M21 12h1M12 3v-1M3 12h-1">
+          <animate
+            fill="freeze"
+            attributeName="stroke-dashoffset"
+            dur="0.2s"
+            values="4;2"
+          />
+        </path>
+        <path d="M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5">
+          <animate
+            fill="freeze"
+            attributeName="stroke-dashoffset"
+            begin="0.2s"
+            dur="0.2s"
+            values="4;2"
+          />
+        </path>
+      </g>
+      <path
+        fill="currentColor"
+        d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
+        opacity="0"
+      >
+        <set attributeName="opacity" begin="0.5s" to="1" />
+      </path>
+    </g>
+    <g fill="currentColor" fillOpacity="0">
+      <path d="m15.22 6.03l2.53-1.94L14.56 4L13.5 1l-1.06 3l-3.19.09l2.53 1.94l-.91 3.06l2.63-1.81l2.63 1.81z">
+        <animate
+          id="lineMdSunnyFilledLoopToMoonFilledLoopTransition0"
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="0.6s;lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+6s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+2.2s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="M13.61 5.25L15.25 4l-2.06-.05L12.5 2l-.69 1.95L9.75 4l1.64 1.25l-.59 1.98l1.7-1.17l1.7 1.17z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+3s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+5.2s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="M19.61 12.25L21.25 11l-2.06-.05L18.5 9l-.69 1.95l-2.06.05l1.64 1.25l-.59 1.98l1.7-1.17l1.7 1.17z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+0.4s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+2.8s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="m20.828 9.731l1.876-1.439l-2.366-.067L19.552 6l-.786 2.225l-2.366.067l1.876 1.439L17.601 12l1.951-1.342L21.503 12z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+3.4s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+5.6s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+    </g>
+    <mask id="lineMdSunnyFilledLoopToMoonFilledLoopTransition1">
+      <circle cx="12" cy="12" r="12" fill="#fff" />
+      <circle cx="22" cy="2" r="3" fill="#fff">
+        <animate
+          fill="freeze"
+          attributeName="cx"
+          begin="0.1s"
+          dur="0.4s"
+          values="22;18"
+        />
+        <animate
+          fill="freeze"
+          attributeName="cy"
+          begin="0.1s"
+          dur="0.4s"
+          values="2;6"
+        />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.1s"
+          dur="0.4s"
+          values="3;12"
+        />
+      </circle>
+      <circle cx="22" cy="2" r="1">
+        <animate
+          fill="freeze"
+          attributeName="cx"
+          begin="0.1s"
+          dur="0.4s"
+          values="22;18"
+        />
+        <animate
+          fill="freeze"
+          attributeName="cy"
+          begin="0.1s"
+          dur="0.4s"
+          values="2;6"
+        />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.1s"
+          dur="0.4s"
+          values="1;10"
+        />
+      </circle>
+    </mask>
+    <circle
+      cx="12"
+      cy="12"
+      r="6"
+      fill="currentColor"
+      mask="url(#lineMdSunnyFilledLoopToMoonFilledLoopTransition1)"
+    >
+      <set attributeName="opacity" begin="0.5s" to="0" />
+      <animate
+        fill="freeze"
+        attributeName="r"
+        begin="0.1s"
+        dur="0.4s"
+        values="6;10"
+      />
+    </circle>
+  </svg>
+);
+
+export const Moon = ({ className, ...rest }) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      {...rest}
+      className={`w-full h-auto ${className}`}
+    >
+      <rect x="0" y="0" width="24" height="24" fill="rgba(255, 255, 255, 0)" />
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeDasharray="2"
+        strokeDashoffset="2"
+        strokeLinecap="round"
+        strokeWidth="2"
+      >
+        <path d="M0 0">
+          <animate
+            fill="freeze"
+            attributeName="d"
+            begin="1.2s"
+            dur="0.2s"
+            values="M12 19v1M19 12h1M12 5v-1M5 12h-1;M12 21v1M21 12h1M12 3v-1M3 12h-1"
+          />
+          <animate
+            fill="freeze"
+            attributeName="stroke-dashoffset"
+            begin="1.2s"
+            dur="0.2s"
+            values="2;0"
+          />
+        </path>
+        <path d="M0 0">
+          <animate
+            fill="freeze"
+            attributeName="d"
+            begin="1.5s"
+            dur="0.2s"
+            values="M17 17l0.5 0.5M17 7l0.5 -0.5M7 7l-0.5 -0.5M7 17l-0.5 0.5;M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5"
+          />
+          <animate
+            fill="freeze"
+            attributeName="stroke-dashoffset"
+            begin="1.5s"
+            dur="1.2s"
+            values="2;0"
+          />
+        </path>
+        <animateTransform
+          attributeName="transform"
+          dur="30s"
+          repeatCount="indefinite"
+          type="rotate"
+          values="0 12 12;360 12 12"
+        />
+      </g>
+      <g fill="currentColor">
+        <path d="M15.22 6.03L17.75 4.09L14.56 4L13.5 1L12.44 4L9.25 4.09L11.78 6.03L10.87 9.09L13.5 7.28L16.13 9.09L15.22 6.03Z">
+          <animate
+            fill="freeze"
+            attributeName="fill-opacity"
+            dur="0.4s"
+            values="1;0"
+          />
+        </path>
+        <path d="M19.61 12.25L21.25 11L19.19 10.95L18.5 9L17.81 10.95L15.75 11L17.39 12.25L16.8 14.23L18.5 13.06L20.2 14.23L19.61 12.25Z">
+          <animate
+            fill="freeze"
+            attributeName="fill-opacity"
+            begin="0.2s"
+            dur="0.4s"
+            values="1;0"
+          />
+        </path>
+      </g>
+      <g
+        fill="currentColor"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <path d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z" />
+        <set attributeName="opacity" begin="0.6s" to="0" />
+      </g>
+      <mask id="lineMdMoonFilledToSunnyFilledLoopTransition0">
+        <circle cx="12" cy="12" r="12" fill="#fff" />
+        <circle cx="18" cy="6" r="12" fill="#fff">
+          <animate
+            fill="freeze"
+            attributeName="cx"
+            begin="0.6s"
+            dur="0.4s"
+            values="18;22"
+          />
+          <animate
+            fill="freeze"
+            attributeName="cy"
+            begin="0.6s"
+            dur="0.4s"
+            values="6;2"
+          />
+          <animate
+            fill="freeze"
+            attributeName="r"
+            begin="0.6s"
+            dur="0.4s"
+            values="12;3"
+          />
+        </circle>
+        <circle cx="18" cy="6" r="10">
+          <animate
+            fill="freeze"
+            attributeName="cx"
+            begin="0.6s"
+            dur="0.4s"
+            values="18;22"
+          />
+          <animate
+            fill="freeze"
+            attributeName="cy"
+            begin="0.6s"
+            dur="0.4s"
+            values="6;2"
+          />
+          <animate
+            fill="freeze"
+            attributeName="r"
+            begin="0.6s"
+            dur="0.4s"
+            values="10;1"
+          />
+        </circle>
+      </mask>
+      <circle
+        cx="12"
+        cy="12"
+        r="10"
+        fill="currentColor"
+        mask="url(#lineMdMoonFilledToSunnyFilledLoopTransition0)"
+        opacity="0"
+      >
+        <set attributeName="opacity" begin="0.6s" to="1" />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.6s"
+          dur="0.4s"
+          values="10;6"
+        />
+      </circle>
+    </svg>
+);
+
+export const LinkedIn = ({ className, ...rest }) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 256 256"
+      {...rest}
+      className={`w-full h-auto ${className}`}
+    >
+      <path fill="none" d="M0 0h256v256H0z" />
+      <g fill="none">
+        
+        <path
+          fill="#fff"
+          d="M184.715 217.685h29.27a4 4 0 0 0 4-3.999l.015-61.842c0-32.323-6.965-57.168-44.738-57.168-14.359-.534-27.9 6.868-35.207 19.228a.32.32 0 0 1-.595-.161V101.66a4 4 0 0 0-4-4h-27.777a4 4 0 0 0-4 4v112.02a4 4 0 0 0 4 4h29.268a4 4 0 0 0 4-4v-55.373c0-15.657 2.97-30.82 22.381-30.82 19.135 0 19.383 17.916 19.383 31.834v54.364a4 4 0 0 0 4 4ZM38 59.627c0 11.865 9.767 21.627 21.632 21.627 11.862-.001 21.623-9.769 21.623-21.631C81.253 47.761 71.491 38 59.628 38 47.762 38 38 47.763 38 59.627Zm6.959 158.058h29.307a4 4 0 0 0 4-4V101.66a4 4 0 0 0-4-4H44.959a4 4 0 0 0-4 4v112.025a4 4 0 0 0 4 4Z"
+        />
+      </g>
+    </svg>
+);
+
+export const Github= ({ className, ...rest }) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 512 512"
+      {...rest}
+      className={`w-full h-auto ${className}`}
+    >
+      <path fill="none" d="M0 0h512v512H0z" />
+      <path
+        fill="currentColor"
+        d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 0 0 3.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 0 1-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0 0 25.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 0 1 5-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 0 1 112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 0 1 5 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 0 0 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32Z"
+      />
+    </svg>
+);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-
+import { Sun, Moon, LinkedIn, Github } from "../assets/icons-svg/icons";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
 
 const Navbar = () => {
 
@@ -10,13 +11,18 @@ const Navbar = () => {
     ]
 
     const [open, setOpen] = useState(false);
-    const [sun, setSun] = useState(false);
+
+
+    const [mode, setMode] = useThemeSwitcher();
 
 
   return (
+
+
+
     <header className="bg-gray-900 shadow-md text-white w-full fixed top-0 left-0 z-[10]">
         
-        <nav className="flex md:flex-row  md:items-center w-[80%] 2xl:w-[70%] m-auto  py-4 ">
+        <nav className="flex md:flex-row  md:items-center w-[80%] 2xl:w-[70%] m-auto items-center  py-4 ">
             <div className="md:w-1/3 p-0 m-0 md:block">
                 <ul className={`md:flex md:items-center absolute md:static bg-white text-black md:bg-transparent md:text-white md:z-auto z-[-1] left-0  md:w-auto md:pl-0 pl-8 w-full  transition-all duration-500 ease-in ${open ? "top-16" : "top-[-350px]"}`}>
                     {
@@ -39,19 +45,19 @@ const Navbar = () => {
                     <span>/&#62;</span>
                 </code>
             </div>
-            <div onClick={()=>setSun(!sun)} className="md:hidden text-2xl absolute right-10">
-                <ion-icon name={sun ? "moon": "sunny"}></ion-icon>
-            </div>  
+            <button onClick={()=>setMode(mode === "light" ? "dark": "light")} className={`md:hidden text-2xl absolute right-10 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark" }`}>
+                {mode ==="dark" ? <Moon/> : <Sun/>}
+            </button>  
             <div className=" flex-grow hidden md:block ">
                 <ul className=" md:flex justify-evenly items-center h-auto text-2xl px-0 ">
-                    <li className="w-6 h-6 md:mx-3 hover:-translate-y-1 cursor-pointer duration-300">
-                        <a href="https://github.com/Lewno" target="_blank" rel="noreferrer"><ion-icon name="logo-github"></ion-icon></a>       
+                    <li className="w-8 h-8 p-1 hover:bg-white hover:text-black rounded-full md:mx-3 cursor-pointer duration-500">
+                        <Github/>   
                     </li>
-                    <li className="w-6 h-6 md:mx-3 hover:-translate-y-1 cursor-pointer duration-300">  
-                        <a href="https://www.linkedin.com/in/carlos-aaron-benites-acevedo/" target="_blank" rel="noreferrer"><ion-icon name="logo-linkedin"></ion-icon></a>
+                    <li className="w-8 h-8 p-1 md:mx-3 cursor-pointer duration-500 rounded-full hover:bg-blue-700">  
+                        <LinkedIn/>
                     </li>
-                    <li onClick={()=>setSun(!sun)} className="w-6 h-6 md:mx-3 hover:-translate-y-1 cursor-pointer duration-300">
-                        <ion-icon name={sun ? "moon": "sunny"}></ion-icon>
+                    <li onClick={()=>setMode(mode === "light" ? "dark": "light")} className={`w-8 h-8 p-1 cursor-pointer   flex items-center justify-center rounded-full ${mode === "light" ? " bg-black text-white" : " bg-white text-black" }`}>
+                    {mode ==="dark" ? <Moon/> : <Sun/>}              
                     </li>
                 </ul>
             </div>
