c9e4abfe0e10a01ba4695ce6f9fbb4e3c23c1ced
lewno <cabax.2002@gmail.com>
2024-09-19 16:05:04 -0300

    feat(all):adding new information

--- src/helper/timeLineEdu.js
+++ src/helper/timeLineEdu.js
@@ -1,8 +1,23 @@
-import unt from "../assets/unt.png"
+import utn from "../assets/utn.jpg"
 
 const timeLineEdu = [
     
     {
+        title:"Ingeniería en Sistemas de Información",
+        subtitle:"Universidad Tecnológica Nacional",
+        date: "Abril 2024 - Presente",
+        icon:utn,
+        name:"Universidad Tecnológica Nacional"
+    },
+    {
+        title:"Cursos en Linea",
+        subtitle:"Udemy, Coursera, HackerRank, etc",
+        decription: "La capacitación no tiene límite. Desde que estoy en el mundo de la programación, he ido ampliando mis conocimientos y habilidades a través de diversos cursos en línea en múltiples plataformas reconocidas como Udemy, Coursera y HackerRank. Estos cursos, me han ayudado a obtener conocimientos actualizados y prácticos en tecnologías y herramientas relevantes para la industria.",
+        date: "Mayo 2022 - presente",
+        icon:"https://raw.githubusercontent.com/Lewno/Porfolio/49d95dbf0cf53cb7164d4d828b0e8cecbf12d989/src/assets/udemy.svg",
+        name:"Udemy, Coursera, HackerRank, etc"
+    },
+ /*   {
         title:"Ingeniería de Sistemas",
         subtitle:"Universidad Nacional de Trujillo",
         decription: "Aprendí lo básico y fundamental de distintos lenguajes de programación como c++, java, c#, php y Javascript. Ademes de gestión de datos a través de MySQL. Esto me permitió obtener bases sólidas en el mundo it, lo cual fue de gran ayuda cuando empezaba aprender diferentes tecnologías.",
@@ -10,14 +25,7 @@ const timeLineEdu = [
         icon:unt,
         name:"Universidad Nacional de Trujillo"
     },
-    {
-        title:"Cursos en Linea",
-        subtitle:"Udemy, Coursera, HackerRank, etc",
-        decription: "La capacitación no tiene límite. Desde que estoy en el mundo de la programación, he ido ampliando mis conocimientos y habilidades a través de diversos cursos en línea en múltiples plataformas reconocidas como Udemy, Coursera y HackerRank. Estos cursos, me han ayudado a obtener conocimientos actualizados y prácticos en tecnologías y herramientas relevantes para la industria.",
-        date: "Mayo 2020 - presente",
-        icon:"https://raw.githubusercontent.com/Lewno/Porfolio/49d95dbf0cf53cb7164d4d828b0e8cecbf12d989/src/assets/udemy.svg",
-        name:"Udemy, Coursera, HackerRank, etc"
-    },
+    */
 ];
 
 export default timeLineEdu
--- src/helper/timeLineExp.js
+++ src/helper/timeLineExp.js
@@ -5,7 +5,7 @@ const timeLineExp = [
         subtitle:"No Country",
         decription1: "Trabajé en estrecha colaboración con el equipo para optimizar el diseño y funcionalidad de la aplicación, aportando significativamente en la interactividad del proyecto",
         decription2:"Identificación y resolución de problemas técnicos de los diversos proyectos, mejorando el rendimiento en un 50% de la aplicación",
-        date: "Julio 2023 - presente",
+        date: "Julio 2023 - Enero 2024",
         icon:"https://raw.githubusercontent.com/Lewno/Porfolio/49d95dbf0cf53cb7164d4d828b0e8cecbf12d989/src/assets/nocountry.svg",
         name:"No Country"
     },
